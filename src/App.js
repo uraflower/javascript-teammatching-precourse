0c71e846315f148faddcaeb71eb6e3341494119d
uraflower <82873315+uraflower@users.noreply.github.com>
2023-12-04 19:28:21 +0900

    feat(앱): 기본 구조 추가 (header, main)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { BASE } from './constants/markup/markup.js';
+
 class App {
+  #app;
+
+  constructor() {
+    this.#app = document.getElementById('app');
+    this.#render();
+  }
+
+  #render() {
+    this.#app.innerHTML += BASE;
+  }
 }
 
 export default App;
---
+++ src/constants/markup/markup.js
@@ -0,0 +1,8 @@
+const BASE = `
+  <header>
+    <h1>우테코 크루와 팀 매칭 관리 보드</h1>
+  </header>
+  <main></main>
+`;
+
+export { BASE };
