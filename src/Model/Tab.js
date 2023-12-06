cae6410397eae697177b0b544a4b32b60a9d9e8b
uraflower <82873315+uraflower@users.noreply.github.com>
2023-12-06 17:30:50 +0900

    feat(탭): TabController에서 직접 탭 전환 제어
    - Tab(탭 모델) 삭제
    - TabController에서 탭 전환 제어 담당

--- src/Model/Tab.js
+++
@@ -1,9 +0,0 @@
-class Tab {
-  #tabs;
-
-  constructor() {
-    this.#tabs = ['crew-tab', 'team-tab'];
-  }
-}
-
-export default Tab;
--- src/controller/TabController.js
+++ src/controller/TabController.js
@@ -1,23 +1,31 @@
-import Tab from '../Model/Tab.js';
 import TabView from '../View/TabView.js';
+import CrewTabController from './CrewTabController.js';
 
 class TabController {
-  #tab;
-
   #view;
 
+  #tabs;
+
+  #crewTabController;
+
   constructor() {
-    this.#tab = new Tab();
+    this.#crewTabController = new CrewTabController();
+
+    this.#tabs = {
+      'crew-tab': this.#crewTabController,
+      'team-tab': null, // 추후 teamTabController 할당
+    };
     this.#view = new TabView();
 
     this.#view.renderTabs();
     this.#view.bindClickTab(this.#handleClickTab);
   }
 
-  #handleClickTab(currentTab) {
-    // tab model control
-    console.log(currentTab);
-  }
+  // view에서 이 함수가 콜백함수로 실행될 때,
+  // this가 이 클래스를 가르키게 하기 위해 화살표 함수 사용
+  #handleClickTab = (currentTab) => {
+    this.#tabs[currentTab].render();
+  };
 }
 
 export default TabController;
